4bdbdeaf96a3203c36fab826cf547b3eab75d28c
Alhada06 <alhada06@gmail.com>
2025-09-24 16:45:57 +0100

    test deploy new actions per nuxt docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  app: {
+    baseURL: "/alhada06.github.io-v2/",
+  },
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui']
-})
+  modules: ["@nuxt/eslint", "@nuxt/test-utils", "@nuxt/ui"],
+});
