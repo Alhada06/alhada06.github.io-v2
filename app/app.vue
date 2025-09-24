046493619d375e5483cad07e9318101491f7f00a
Alhada06 <alhada06@gmail.com>
2025-09-24 18:33:43 +0100

    test nuxt ui configs

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div>
+      <!-- <NuxtRouteAnnouncer />
+      <NuxtWelcome /> -->
+      <UColorModeButton />
+    </div>
 
-  <div class="text-3xl font-bold underline bg-amber-700">
-    teste deploy automations
-  </div>
+    <div class="text-3xl font-bold underline text-blue-500 bg-amber-700">
+      teste deploy automations
+    </div>
+  </UApp>
 </template>
