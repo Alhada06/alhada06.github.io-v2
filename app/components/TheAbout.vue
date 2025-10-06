52f8a26a05ebe75d9a311364c5237a12f9a9be27
Alhada06 <alhada06@gmail.com>
2025-10-06 17:29:13 +0100

    basic layout and about component created and all graphql queries and fragments documents created

---
+++ app/components/TheAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <UPageCard
+    variant="outline"
+    :title="$t('about')"
+    :description="about"
+    spotlight
+    spotlight-color="secondary"
+    orientation="vertical"
+  />
+</template>
+
+<script lang="ts" setup>
+import aboutQuery from "@/graphql/queries/about.query.gql";
+ const {locale} = useI18n();
+const {result:aboutData}= useQuery(aboutQuery,()=>{
+  return {locale: locale.value} ;
+});
+const about = computed(() => aboutData.value?.about?.description);
+</script>
