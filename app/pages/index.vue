22dfc10df6c29b1001ca848973df2ff1e3581341
Alhada06 <alhada06@gmail.com>
2025-10-07 16:48:40 +0100

    test 2 ssr

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <UPageSection id="section1" class="w-full h-full">
+    <UPageSection id="section1" ref="section1" class="w-full h-full">
       <TheAbout />
     </UPageSection>
-    <UPageSection id="section2">
+    <UPageSection id="section2" ref="section2" class="w-full h-full">
       <UPageCard
         variant="outline"
         spotlight
@@ -11,19 +11,14 @@
         orientation="horizontal"
       >
         Section 2 ----------------------------------------
-        -------------------------------- test if compile live prod
+        -------------------------------- test if compile live prod 2
         ---------------------------------------- -
         ----------------------------------------------
         --------------------------------------------</UPageCard
       >
     </UPageSection>
-    <UPageSection id="section3">
-      <UPageCard
-        variant="outline"
-        spotlight
-        spotlight-color="secondary"
-        orientation="horizontal"
-      >
+    <UPageSection id="section3" ref="section3" class="w-full h-full">
+      <UPageCard variant="outline" spotlight spotlight-color="secondary">
         Section 3 ----------------------------------------
         --------------------------------
         ---------------------------------------- -
@@ -31,13 +26,8 @@
         --------------------------------------------</UPageCard
       >
     </UPageSection>
-    <UPageSection id="section4">
-      <UPageCard
-        variant="outline"
-        spotlight
-        spotlight-color="secondary"
-        orientation="horizontal"
-      >
+    <UPageSection id="section4" ref="section4" class="w-full h-full">
+      <UPageCard variant="outline" spotlight spotlight-color="secondary">
         Section 4 ----------------------------------------
         --------------------------------
         ---------------------------------------- -
@@ -45,13 +35,8 @@
         --------------------------------------------</UPageCard
       >
     </UPageSection>
-    <UPageSection id="section5">
-      <UPageCard
-        variant="outline"
-        spotlight
-        spotlight-color="secondary"
-        orientation="horizontal"
-      >
+    <UPageSection id="section5" ref="section5" class="w-full h-full">
+      <UPageCard variant="outline" spotlight spotlight-color="secondary">
         Section 5 ----------------------------------------
         --------------------------------
         ---------------------------------------- -
